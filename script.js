264f34c6c9d9e4e4f3b3900c56775239c57ad094
Felipesilva94 <lippepunhal@gmail.com>
2022-06-13 23:19:15 -0300

    desafio 02 js - Vai na Web

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+// 1 - Criar um loop que conte de 1 até 10 usando FOR
+
+let number = 1;
+for(let number = 1; number <=100; number++) {
+    console.log(`resposta 1º pergunta: ${number}`)
+}
+
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+let numero = 10;
+while(numero >=1){
+    console.log(`resposta 2º pergunta : ${numero}`)
+ numero--;
+}
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+ let numberImpar = 1;
+ for(let numberImpar = 1; numberImpar <= 100; numberImpar +=2){
+    console.log(`loop de números de 1 á 100 (impar): ${numberImpar}`)
+ }
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+let numberPar = 0;
+ for(let numberPar = 0; numberPar <= 100; numberPar +=2){
+    console.log(`loop de números de 1 á 100 (Par): ${numberPar}`)
+ }
+
+
+ 
